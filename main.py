c877c4af22c1aca4b7c8c7cdcdd3d78cbf8549eb
Kerem Celik <31195@isw.info>
2022-10-22 23:19:54 +0200

    update
    added name

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ screen_fill = 50, 50, 50
 window_size = 800
 screen = pg.display.set_mode([window_size] * 2)
 
+pg.display.set_caption("snake")
+
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
