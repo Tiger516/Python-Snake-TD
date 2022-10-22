0bb305bd8ec16360565e6cce334d39bd15d70adf
Kerem Celik <31195@isw.info>
2022-10-22 21:50:14 +0200

    Update
    removed captions because they sucked and made my bain hurt

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 import pygame
+import images
 
-# choosing the background color
 background_colour = (50, 50, 50)
-
-# making the screen var
 screen = pygame.display.set_mode((500, 500))
 
-# naming the window
 pygame.display.set_caption('Snake')
+pygame.display.set_icon()
 
-# filling the background up with the colour you put in the var
 screen.fill(background_colour)
-# flips the display
+
 pygame.display.flip()
 
-# sets running as true
 running = True
- 
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
