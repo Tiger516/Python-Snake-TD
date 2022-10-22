a354cc27f48c044c013381485ea232c071f1211f
Kerem Celik <31195@isw.info>
2022-10-22 21:23:45 +0200

    update
    added captions

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import pygame
 
-background_colour = (0, 0, 0)
+# choosing the background color
+background_colour = (50, 50, 50)
+
+# making the screen var
 screen = pygame.display.set_mode((500, 500))
+
+# naming the window
 pygame.display.set_caption('Snake')
 
+# filling the background up with the colour you put in the var
 screen.fill(background_colour)
+# flips the display
 pygame.display.flip()
 
+# sets running as true
 running = True
  
 while running:
