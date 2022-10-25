768474324475fb36d6484e76adc8d32bbc21448d
Kerem Celik <31195@isw.info>
2022-10-26 00:58:10 +0200

    Update
    Made it as pg

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-import pygame
+from lib2to3 import pgen2
+import pygame as pg
 
 Window_Size = 500
 Window_Game_Name = "Snake"
 running = True
 background_colour = (50, 50, 50)
-screen = pygame.display.set_mode([Window_Size] * 2)
-pygame.display.set_caption(Window_Game_Name)
+screen = pg.display.set_mode([Window_Size] * 2)
+pg.display.set_caption(Window_Game_Name)
 
 screen.fill(background_colour)
-pygame.display.flip()
+pg.display.flip()
 
 while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
             running = False
