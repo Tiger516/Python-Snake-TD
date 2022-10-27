007e4c5677d53cba17c6957dc395428763c293ba
deatg5 <67492418+deatg5@users.noreply.github.com>
2022-10-27 00:39:48 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
+import imp
 import pygame as pg
 import os
+import random
+from random import randint
 
-Window_Size = 500
-Window_Game_Name = "Snake"
+window_width = 500
+window_height = 500
+game_display = pg.display.set_mode((window_width, window_height))
+window_game_name = "Snake"
 running = True
 background_colour = (50, 50, 50)
-screen = pg.display.set_mode([Window_Size] * 2)
-pg.display.set_caption(Window_Game_Name)
+screen = pg.display.set_mode((window_width, window_height))
+pg.display.set_caption(window_game_name)
 
 screen.fill(background_colour)
 pg.display.flip()
 
-Apple = pg.image.load(os.path.join('images', 'apple.png'))
+apple = pg.image.load(os.path.join('images', 'apple.png'))
+apple = pg.transform.scale(apple, (32, 32)) 
+
+def create_apple(x,y):
+    game_display.blit(apple, (x,y))
+
+
+create_apple(randint(0, window_width - 32), randint(0, window_height - 32))
+    
+pg.display.update()
 
 while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
+            
