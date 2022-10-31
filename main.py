f3be08204a67087ae4ca6a1963943341e8d735ab
Kerem Celik <31195@isw.info>
2022-10-31 20:02:40 +0100

    Update
    AUAIIAIA

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import imp
 import pygame as pg
 import os
+import time
 import random
 from random import randint
+from time import sleep
+from pygame.locals import * 
 
 window_width = 500
 window_height = 500
@@ -35,9 +38,9 @@ def create_apple(x,y):
 def create_head(x,y):
     game_display.blit(snake_head, (x,y))
 
-
+create_head(randint(0, window_width - 32), randint(0, window_height - 32))
 create_apple(randint(0, window_width - 32), randint(0, window_height - 32))
-    
+
 pg.display.update()
 
 pg.display.set_icon(apple)
